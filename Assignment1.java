5289e89da9b8d38e0cb02907f02fd88500f79d49
Kianrotzen <111473023+Kianrotzen@users.noreply.github.com>
2022-08-22 10:07:54 +0800

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
+
+system.out.printlnx("Hello, I am Genesis")
+
+sytem.out.printlnx("Gensin Impact ") 
